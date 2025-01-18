a6672841e868396454626f6ca0437778488f7550
tunahanunver <tunahanunver26@gmail.com>
2025-01-18 18:52:38 +0300

    UI Manager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using Signals;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SubscribeEvents();    
+    }
+
+    private void SubscribeEvents()
+    {
+        CoreGameSignals.Instance.onLevelInitialize += OnLevelInitialize;
+        CoreGameSignals.Instance.onLevelSuccessful += OnLevelSuccessful;
+        CoreGameSignals.Instance.onLevelFailed += OnLevelFailed;
+        CoreGameSignals.Instance.onReset += OnReset;
+    }
+
+    private void OnLevelFailed()
+    {
+        CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Fail, 2);
+    }
+
+    private void OnLevelSuccessful()
+    {
+        CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Win, 2);
+    }
+
+    private void OnLevelInitialize(byte arg0)
+    {
+        CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Level, 0);
+        UISignals.Instance.onSetLevelValue?.Invoke((byte)CoreGameSignals.Instance.onGetLevelValue?.Invoke());
+    }
+
+    private void OnReset()
+    {
+        CoreUISignals.Instance.onCloseAllPanels?.Invoke();
+        CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Start, 1);
+    }
+}
